9aca3ade05de46e9bbb2962158dafbbc66d7d9ff
diego_alexander <diegocaste7@hotmail.com>
2022-02-22 11:24:00 -0500

    primer commit, se realiza logica para obtener datos por web scraping usando xpath, se adiciona archivo raw como muestra del resultado

---
+++ data/src/scrapy_process.py
@@ -0,0 +1,84 @@
+# importar las librerias necesarias para hacer el proyecto
+import requests
+import lxml.html as html
+import os
+import datetime
+import random
+
+#variables estaticas que usaremos
+URL_ORIGIN = 'https://www.properati.com.co/s/bucaramanga/apartamento/arriendo?page='
+DOMAIN = 'https://www.properati.com.co'
+INMUEBLES = '//h2/ancestor::a/@href'
+
+TITLE = '//h1/text()'
+PRICE = '//div[contains(@class,"MoneyBlock")]/div[position()=1]/span/text()'
+DESCRIPTION = '//div[@class="child-wrapper"]/div[position()=1]/text()'
+UNIQUE_ID = '//div[@class="child-wrapper"]/div[position()=2]/text()'
+ADD_INFO= '//div[contains(@class,"ContentFe")]/div/div/ul/li/text()'
+CHARACTERISTICS = '//div[contains(@class,"StyledTypology")]/div/div[position()=2]/span/text()'
+
+def descripcion_inmueble(link):
+    try:
+        response = requests.get(DOMAIN + link)
+        if response.status_code == 200:
+            predio = response.content.decode('utf-8')
+            parsed = html.fromstring(predio)
+            
+            try:
+
+                unique_id = parsed.xpath(UNIQUE_ID)[1]
+                title = parsed.xpath(TITLE)[0]
+                price = parsed.xpath(PRICE)[2]
+                descrip = ' '.join(parsed.xpath(DESCRIPTION))
+                add_info = '--'.join(parsed.xpath(ADD_INFO))
+                charact = '--'.join(parsed.xpath(CHARACTERISTICS))
+
+
+            except IndexError:
+                return
+
+            with open('../raw/data_raw.txt', 'a', encoding='utf-8') as f:
+                f.write('"' + unique_id + '","' + title + '","' + price + '","' + descrip + '","' + add_info + '","' + charact + '"')
+                f.write('\n')
+                f.close()
+
+
+        else:
+            raise ValueError(f'Error: {response.status_code}')
+    except ValueError as ve:
+        print(ve)
+
+
+
+def enter_home():
+
+    #vamos a obtener el numero de paginas deseado para la consulta
+    for n_page in range(0,30):
+        #try catch para manejar errores
+        try:
+            # hacemos requerimiento a la pagin
+            response = requests.get(URL_ORIGIN+str(n_page))
+            # si nos da una respuesta positiva
+            if response.status_code == 200:
+                home = response.content.decode('utf-8')
+                parsed = html.fromstring(home)
+                url_inmueble = parsed.xpath(INMUEBLES)
+                
+                # crear archivo csv
+                for link in url_inmueble:
+                    descripcion_inmueble(link)
+
+            # si no, entonces enviar error al except
+            else:
+                raise ValueError(f'Error: {response.status_code}')
+        #mostrar error del try
+        except ValueError as ve:
+            print(ve)
+
+
+
+def run():
+    enter_home()
+
+if __name__ == '__main__':
+    run()
